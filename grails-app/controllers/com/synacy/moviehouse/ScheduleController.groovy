66c23097011efb11656e1fa0b71dbf9372ceebec
Kenichi Go Uang <kenichi.uang@synacy.com>
2017-05-23 19:25:26 +0800

    Refactor controller to accept date parameter with new service method
    Signed-off-by: Kenichi Go Uang <kenichi.uang@synacy.com>

--- grails-app/controllers/com/synacy/moviehouse/ScheduleController.groovy
+++ grails-app/controllers/com/synacy/moviehouse/ScheduleController.groovy
@@ -1,5 +1,8 @@
 package com.synacy.moviehouse
 
+import com.synacy.moviehouse.exception.MissingParameterException
+import org.springframework.http.HttpStatus
+
 class ScheduleController {
 
     static responseFormats = ['json']
@@ -10,6 +13,17 @@ class ScheduleController {
 
         String date = params.date ? String.valueOf(params.date) : null
 
-        List<Schedule> schedules = scheduleService.fetchAllSchedules(date)
+        if(!date) {
+            throw new MissingParameterException("Missing date parameter in URI.")
+        }
+
+        List<Schedule> schedules = scheduleService.fetchAllSchedulesByDate(date)
+
+        respond(schedules)
+    }
+
+    def handleMissingParameterException(MissingParameterException e) {
+        response.status = HttpStatus.BAD_REQUEST.value()
+        respond([error: e.getMessage()])
     }
 }
