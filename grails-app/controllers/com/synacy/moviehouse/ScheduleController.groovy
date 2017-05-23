ca451fdf43d188d5fb07f9e3c4431e45f9350766
Kenichi Go Uang <kenichi.uang@synacy.com>
2017-05-23 19:22:49 +0800

    Create controller for Schedule
    Signed-off-by: Kenichi Go Uang <kenichi.uang@synacy.com>

--- grails-app/controllers/com/synacy/moviehouse/ScheduleController.groovy
+++ grails-app/controllers/com/synacy/moviehouse/ScheduleController.groovy
@@ -2,5 +2,14 @@ package com.synacy.moviehouse
 
 class ScheduleController {
 
-    def index() { }
+    static responseFormats = ['json']
+
+    ScheduleService scheduleService
+
+    def fetchSchedules() {
+
+        String date = params.date ? String.valueOf(params.date) : null
+
+        List<Schedule> schedules = scheduleService.fetchAllSchedules(date)
+    }
 }
