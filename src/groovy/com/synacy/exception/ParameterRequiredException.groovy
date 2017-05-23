b0fd187a6dab7e973bc41ea5fef21a6eaa74958b
sbanjoe25 <sbanjoe25@gmail.com>
2017-05-23 18:05:11 +0800

    add ParameterRequiredException
    Signed-off-by: sbanjoe25 <sbanjoe25@gmail.com>

---
+++ src/groovy/com/synacy/exception/ParameterRequiredException.groovy
@@ -0,0 +1,8 @@
+package com.synacy.exception
+
+class ParameterRequiredException extends RuntimeException{
+
+    public ParameterRequiredException(String message) {
+        super(message)
+    }
+}
